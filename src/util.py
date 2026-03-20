59d8f46586041606a3db2f7d212e62d630596a16
Francisco Ganacim <ganacim@impa.br>
2026-03-20 08:11:52 -0300

    Add create_if_missing option to data_path function

--- src/util.py
+++ src/util.py
@@ -1,7 +1,9 @@
 import importlib.resources as res
 from pathlib import Path
 
-def data_path(dataset_name=None) -> Path:
+def data_path(dataset_name=None, create_if_missing=False) -> Path:
+    """Returns the path to the data directory. If dataset_name is provided, returns the path to the dataset directory.
+    If create_if_missing is True, creates the directory if it does not exist."""
     # get package name
     pkg_name = __name__.split(".")[0]
     # get package path
@@ -9,6 +11,11 @@ def data_path(dataset_name=None) -> Path:
         data_path = "/".join(str(path).split("/")[:-1])
     # return data path
     if dataset_name:
-        return Path(data_path) / "data" / dataset_name
+        path = Path(data_path) / "data" / dataset_name
     else:
-        return Path(data_path) / "data"
+        path = Path(data_path) / "data"
+    
+    if create_if_missing and not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
+
+    return path
